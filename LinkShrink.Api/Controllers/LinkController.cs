bdbfe40bc87456ace9226c3afdac1353b066e1f2
Alkamal30 <yurevich.vlad3005@gmail.com>
2024-02-06 22:16:30 +0300

    Add main LinkController with simple logic

---
+++ LinkShrink.Api/Controllers/LinkController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkShrink.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LinkController : ControllerBase
+{
+    private static List<string> _links = new List<string>();
+
+    [HttpGet]
+    public async Task<IActionResult> GetUrl(string url)
+    {
+        if (_links.Contains(url))
+        {
+            return Redirect("https://microsoft.com");
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUrl([FromBody] string url)
+    {
+        if(string.IsNullOrEmpty(url))
+        {
+            return BadRequest();
+        }
+
+        _links.Add(url);
+
+        return Ok();
+    }
+}
