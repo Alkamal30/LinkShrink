a0e5ebd740a0c09f02ee9723442a1951a99ac60a
Alkamal30 <yurevich.vlad3005@gmail.com>
2024-02-08 00:06:00 +0300

    Add Link model

---
+++ LinkShrink.Api/Models/Link.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkShrink.Api.Models;
+
+public class Link
+{
+    public long Id { get; set; }
+    public string RedirectUrl { get; set; } = string.Empty;
+    public string OriginUrl { get; set; } = string.Empty;
+}
