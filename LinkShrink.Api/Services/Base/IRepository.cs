f22c064cf3be55501feb55cc87dc6b97a8e506a1
Alkamal30 <yurevich.vlad3005@gmail.com>
2024-02-16 14:44:48 +0300

    Add IRepository interface

---
+++ LinkShrink.Api/Services/Base/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using LinkShrink.Api.Models.Base;
+
+namespace LinkShrink.Api.Services.Base;
+
+public interface IRepository<TModel, TId> where TModel : BaseModel<TId>
+{
+    Task<TModel> GetByIdAsync(TId id);
+    Task<IEnumerable<TModel>> GetAllAsync();
+
+    Task AddAsync(TModel entity);
+    Task UpdateAsync(TModel entity);
+    Task RemoveAsync(TModel entity);
+    Task RemoveByIdAsync(TId id);
+}
